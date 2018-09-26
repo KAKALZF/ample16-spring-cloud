50cb03e7d0b9b091030d9a1c354b7a47ee544919
KAKALZF <575800503@qq.com>
2018-09-26 15:46:59 +0800

    新建项目

---
+++ consumer/src/main/java/com/ample16/springcloud/consumer/ConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.ample16.springcloud.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+}
